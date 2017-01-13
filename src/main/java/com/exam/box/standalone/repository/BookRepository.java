66d46aabe00b6e54483768be1d188422a31bcb6f
deuksu.bang <deuksu.bang@gmail.com>
2017-01-13 11:13:55 +0900

    move to exam-noxml repo, commit

--- src/main/java/com/exam/box/standalone/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exam.box.standalone.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.exam.box.standalone.domain.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
